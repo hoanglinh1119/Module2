33bbacb9892ec4d5a17747e62c458899810074f8
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-24 00:12:10 +0700

    12121

---
+++ src/com/company/Usd.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Usd {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        long usd,vnd,tigia;
+        tigia=23000;
+        System.out.print("nhap so tien usd vao:");
+        usd=scanner.nextLong();
+        vnd=usd*tigia;
+        System.out.print("so tien vnd la:"+vnd);
+
+    }
+}
