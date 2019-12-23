78fd778ac05d940f5a1e64254d6c3c72f79f0ef5
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-24 00:03:52 +0700

    12121

---
+++ src/com/company/numbervoid.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class numbervoid {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        int number;
+        System.out.print("nhap so ban muon doc:");
+        number=scanner.nextInt();
+        if(number>=0&&number<=15){
+            switch(number){
+                case 0:
+                    System.out.print("zero");
+                    break;
+                case 1:
+                    System.out.print("one");
+                    break;
+                case 2:
+                    System.out.print("two");
+                    break;
+                case 3:
+                    System.out.print("three");
+                    break;
+                case 4:
+                    System.out.print("four");
+                    break;
+                case 5:
+                    System.out.print("five");
+                    break;
+                case 6:
+                    System.out.print("six");
+                    break;
+                case 7:
+                    System.out.print("seven");
+                    break;
+                case 8:
+                    System.out.print("eight");
+                    break;
+                case 9:
+                    System.out.print("nine");
+                    break;
+                case 10:
+                    System.out.print("ten");
+                    break;
+                case 11:
+                    System.out.print("eleven");
+                    break;
+                case 12:
+                    System.out.print("twelve");
+                    break;
+                case 13:
+                    System.out.print("thirteen");
+                    break;
+                case 14:
+                    System.out.print("fourteen");
+                    break;
+                case 15:
+                    System.out.print("fifteen");
+                    break;
+
+            }
+        } else if(number>=16&&number<=19){
+            switch (number){
+                case 16:
+
+            }
+        }
+    }
+}
