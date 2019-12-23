76b34607d55514d2df9f79b97ad1dd02ffdba683
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-23 23:09:00 +0700

    12121

---
+++ src/com/company/bmi.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class bmi {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        double weight,height,bmi;
+        System.out.print("your weight(in kilogam):");
+        weight=scanner.nextDouble();
+        System.out.print("your height(in metter):");
+        height=scanner.nextDouble();
+        bmi=weight/Math.pow(height,2);
+        if(bmi<18.5)
+            System.out.printf("%f%s",bmi,"under weight");
+        else if(bmi<25.0)
+            System.out.printf("%f%s",bmi,"Normal");
+        else if(bmi<30.0)
+            System.out.printf("%f%s",bmi,"over weight");
+        else System.out.printf("%f%s",bmi,"obese");
+    }
+}
