22bc98698e24f533332b29d5c731532e6e82bb2b
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-24 15:37:44 +0700

    12121

---
+++ src/com/company/hien_thi_SNT.java
@@ -0,0 +1,20 @@
+package com.company;
+public class hien_thi_SNT {
+    public static void main(String[]args){
+        int i=2;
+       for (i=2;i<101;i++){
+           if(songuyento(i)){
+               System.out.println(i);
+           }
+       }
+    }
+
+    public static boolean songuyento(int number){
+        for (int j=2;j<=Math.sqrt(number);j++){
+          if(number%j==0){
+              return  false;
+          }
+        }
+        return true;
+    }
+}
