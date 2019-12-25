9cc19edb9b78687292e03dcb5efe0f0a3d1a2820
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-25 16:29:36 +0700

    12121

---
+++ src/project/valueMaxOfArray.java
@@ -0,0 +1,40 @@
+package project;
+
+import java.util.Scanner;
+
+public class valueMaxOfArray {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner=new Scanner(System.in);
+        do{
+            System.out.print("Enter a size:");
+            size=scanner.nextInt();
+            if(size>20){
+                System.out.print("Size does not exceed 20");
+            }
+        }while (size>20);
+        array=new int[size];
+        int i=0;
+        while (i<array.length){
+            System.out.print("Enter element" +(i+1)+":");
+            array[i]=scanner.nextInt();
+            i++;
+        }
+        System.out.printf("-20s%s","Propety list:","");
+        for (int j=0;j<array.length;j++){
+            System.out.print(array[j]+"\t");
+        }
+        int max=array[0];
+        int index=1;
+        for (int j=0;j<array.length;j++){
+             if(array[j]>max){
+                 max=array[j];
+                 index=j+1;
+             }
+        }
+        System.out.print("value max of Array: "+max+" position "+index);
+
+
+    }
+}
