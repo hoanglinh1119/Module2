1d87d7a8394c73a1166aea418e1cd1f0e2fe3615
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-25 16:54:14 +0700

    12121

---
+++ src/project/convertTemperature.java
@@ -0,0 +1,36 @@
+package project;
+
+import java.util.Scanner;
+
+public class convertTemperature {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("menu");
+        System.out.println("1.Celsius to fahreheit");
+        System.out.println("2.fahrenheit to celsius");
+        System.out.println("Enter your choice:");
+        int choice=scanner.nextInt();
+        switch (choice){
+            case 1:
+                System.out.print("Enter a value of celsius:");
+                double celsius=scanner.nextDouble();
+                System.out.println("Celsius to fahreheit :"+celsiustofahreheit(celsius));
+                break;
+            case 2:
+                System.out.print("Enter a value of Fahreheit:");
+                double fahreheit=scanner.nextDouble();
+                System.out.println("Fahreheit to Celsius:"+fahreheittocelsius(fahreheit));
+                break;
+        }
+
+
+    }
+    public static double celsiustofahreheit(double celsius){
+        double fahreheit=(9.0/5)*celsius+32;
+        return fahreheit;
+    }
+    public static double fahreheittocelsius(double fahreheit){
+        double celsius=(5.0/9)*(fahreheit-32);
+        return celsius;
+    }
+}
