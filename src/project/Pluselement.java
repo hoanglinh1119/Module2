7addee5dfff777ffc2bc802115ff620b29368bc2
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-26 09:23:46 +0700

    12121

--- src/project/Pluselement.java
+++ src/project/Pluselement.java
@@ -10,18 +10,18 @@ public class Pluselement {
         int index = scanner.nextInt();
         System.out.print("input value of index:");
         int value=scanner.nextInt();
-        System.out.printf("%-12%s%s", "Array after plus element:", "");
-        for (int i=0;i<array.length;i++) {
-            if (i >= index) {
+        for (int i=array.length-1;i>=index;i--) {
                 array[i] = array[i - 1];
-                System.out.print(array[i] + "\t");
-            } else if (i == index - 1) {
-                array[i] = value;
-                System.out.print(array[i] + "\t");
-            } else {
-                System.out.print(array[i] + "\t");
-            }
-
         }
+       System.out.printf("%-12%s%s", "Array after plus element:", "");
+        for (int i=0;i<array.length;i++){
+            if(i==index-1){
+                array[i]=value;
+                System.out.print(array[i]+"\t");
+            }else {
+                System.out.print(array[i]+"\t");
+            }
+        }
+
     }
 }
