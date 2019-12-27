d2daf4622e52275c662c5a9bcb1ade75df10e055
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-27 13:45:46 +0700

    12121

---
+++ src/project/AccessModifier.java
@@ -0,0 +1,29 @@
+package project;
+
+ class AccessModifier {
+
+
+     private int data = 40;
+
+     private void msg() {
+         System.out.println("Hello java");
+     }
+
+ }
+
+
+
+    public class Simple{
+
+        public static void main(String args[]){
+
+            AccessModifier obj=new AccessModifier();
+
+            System.out.println(obj.data);//Compile Time Error
+
+            obj.msg();//Compile Time Error
+
+        }
+
+    }
+
---
+++ src/project/So_luong_sv.java
@@ -0,0 +1,7 @@
+package project;
+
+public class So_luong_sv {
+    public static void main(String[] args) {
+        
+    }
+}
