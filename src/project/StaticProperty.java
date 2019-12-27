76eaa43b230f32435ec6bee70ae3bacf7dcfc255
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2019-12-27 14:36:24 +0700

    12121

---
+++ src/project/StaticProperty.java
@@ -0,0 +1,23 @@
+package project;
+
+public class StaticProperty {
+
+    private String name;
+
+    private String engine;
+
+    public static int numberOfCars;
+
+    public StaticProperty(String name, String engine) {
+
+        this.name = name;
+
+        this.engine = engine;
+
+        numberOfCars++;
+
+    }
+
+
+}
+
---
+++ src/project/TestStaticProperty.java
@@ -0,0 +1,18 @@
+package project;
+
+public class TestStaticProperty {
+
+    public static void main(String[] args) {
+
+        StaticProperty car1 = new StaticProperty("Mazda 3","Skyactiv 3");
+
+        System.out.println(StaticProperty.numberOfCars);
+
+        StaticProperty car2 = new StaticProperty("Mazda 6","Skyactiv 6");
+
+        System.out.println(StaticProperty.numberOfCars);
+
+    }
+
+}
+
