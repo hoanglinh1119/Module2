85dc1f2352d0d9f46f9aab8334a97078c0f70a1c
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2020-01-06 14:26:52 +0700

    12121

---
+++ src/com/Circle.java
@@ -0,0 +1,47 @@
+package com;
+
+public class Circle {
+    private String color="green";
+    private boolean filled=true;
+    private double radius=1.0;
+    public Circle(){
+    }
+    public Circle(String color,boolean filled,double radius){
+        this.color=color;
+        this.filled=filled;
+        this.radius=radius;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor( String color){
+        this.color=color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+    public void setFilled(boolean filled){
+        this.filled=filled;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public void setRadius(double radius){
+        this.radius=radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    @Override
+    public String toString(){
+        return "A shape with color of "
+                +getColor()+"\n"
+                +"and"
+                +(isFilled()?"filled":"not filled")
+                +"A circle with radius = "
+                +getRadius()+"\n"
+                +"the area of circle = "
+                +getArea();
+    }
+}
---
+++ src/com/Cylinder.java
@@ -0,0 +1,25 @@
+package com;
+
+public class Cylinder extends Circle{
+    double height=2.0;
+    double volume;
+    public Cylinder(){
+    }
+    public Cylinder(String color,boolean filled,double radius,double height){
+        super(color,filled,radius);
+        this.height=height;
+    }
+    public double getVolume(){
+        return super.getArea()*height;
+    }
+    @Override
+    public String toString(){
+        return "volume of Cylinder ="
+                +getVolume()+"\n"
+                +super.toString();
+    }
+    public static void main(String[] args) {
+        Circle cylinder=new Cylinder("green",true,2.0,3.0);
+        System.out.println(cylinder);
+    }
+}
