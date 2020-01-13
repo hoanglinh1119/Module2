da901a4744feb5c856c1a75f81ba3eb2ea1ba947
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2020-01-13 19:21:06 +0700

    12121

--- src/Arrays/StackInJava/Palindrome.java
+++ src/Arrays/StackInJava/Palindrome.java
@@ -6,11 +6,11 @@ public class Palindrome {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("nhap chuoi cua ban vao: ");
-        String chuoi=scanner.nextLine();
+        String input=scanner.nextLine();
         int count=0;
-        char[] strings=new char[chuoi.length()];
-        for (int i=0;i<chuoi.length();i++){
-            strings[i]=chuoi.charAt(i);
+        char[] strings=new char[input.length()];
+        for (int i=0;i<input.length();i++){
+            strings[i]=input.charAt(i);
             count++;
         }
         Stack<Character> islandString=new Stack<Character>();
@@ -18,10 +18,29 @@ public class Palindrome {
         for (int i=0;i<count;i++){
             islandString.push(strings[i]);
         }
+        char[] islandStrig=new char[count];
         for (int i=0;i<count;i++){
-            islandString.pop();
+            islandStrig[i]=islandString.pop();
         }
         Queue<Character> island=new LinkedList<>();
-
+        for (int i = 0; i<count; i++){
+            island.add(strings[i]);
+        }
+        char[] notIsland=new char[count];
+        for (int i=0;i<count;i++){
+            notIsland[i]=island.poll();
+        }
+        boolean isPalindrome=true;
+        for (int i=0;i<count;i++){
+            if(islandStrig[i]!=notIsland[i]){
+                isPalindrome=false;
+                break;
+            }
+        }
+        if (isPalindrome){
+            System.out.println("The string is a palindrome");
+        }else {
+            System.out.println("The string is not a palindrome");
+        }
     }
 }
