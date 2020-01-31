5447b9ff4d8c5c1029cb0bed6f76fe6141bbe359
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2020-01-31 16:54:55 +0700

    12121

---
+++ src/Regex/CrawlNewSpaper.java
@@ -0,0 +1,25 @@
+package Regex;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNewSpaper {
+    public static void main(String[] args) throws IOException {
+        URL url=new URL("https://dantri.com.vn/the-gioi.htm");
+        Scanner scanner=new Scanner(new InputStreamReader(url.openStream()));
+        scanner.useDelimiter("\\Z");
+        String content=scanner.next();
+        scanner.close();
+        content=content.replaceAll("//n+"," ");
+        Pattern p=Pattern.compile("<b> (.*?)</b>");
+        Matcher m=p.matcher(content);
+        while (m.find()){
+            System.out.println(m.group(1));
+        }
+    }
+}
