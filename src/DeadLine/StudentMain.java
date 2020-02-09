f74ae3eb07485a9c149d89b62cd42972b8922d6b
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2020-02-09 18:30:25 +0700

    12121

--- src/DeadLine/StudentMain.java
+++ src/DeadLine/StudentMain.java
@@ -21,18 +21,19 @@ public class StudentMain {
         StudentMethod studentMethod=new StudentMethod();
 
         studentMethod.readFile(arrayList,srcInput);
-        System.out.println(" hien thi tim theo id");
-       studentMethod.searchId(arrayList,scanner);
         System.out.println("hien thi nhung nguoi co diem tren 85");
         studentMethod.searchScoreBigger85(arrayList);
         System.out.println(("hien thi theo ho nguyen"));
         studentMethod.searchNameFollowSurname(arrayList);
         System.out.println("hien thi dinh dang email sai ");
         studentMethod.searchFollowFormatEmail(arrayList);
+        System.out.println(" hien thi tim theo id");
+        studentMethod.searchId(arrayList,scanner);
         System.out.println("file sap xep theo diem va luu file");
         studentMethod.sortFollowScoreDown(arrayList,srcOutputFollowArrangeScore);
         System.out.println("file sap xep theo ten va luu file ");
         studentMethod.sortFollowName(arrayList,srcOutputFollowName);
+
         System.out.println(" ban co muon them sua xoa khong");
         System.out.println("them enter 1");
         System.out.println("sua enter 2");
